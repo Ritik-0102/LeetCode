b04aa872ad7da3f840bfc9574948eb9612d8b013
Ritik Rana <ritikrana.dev@gmail.com>
2026-09-11 00:24:34 +0530

    [1657] [Time Beats: 66.27%] [Memory Beats: 84.03%] - LeetPush

--- 1657-determine-if-two-strings-are-close/1657-Determine-if-Two-Strings-Are-Close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-Determine-if-Two-Strings-Are-Close.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+        if(word1.size() != word2.size()){
+            return false;
+        }
+        
         vector<int> freq1(26,0);
         vector<int> freq2(26,0);
-        unordered_map<int,bool> m1;
-        unordered_map<int,bool> m2;
 
         for(char ch:word1){
             freq1[ch - 'a']++;
-            m1[ch - 'a'] = true;
         }
 
         for(char ch:word2){
             freq2[ch - 'a']++;
-            m2[ch - 'a'] = true;
         }
 
         for(int i=0;i<26;i++){
-            for(int j=0;j<26;j++){
-                if(freq1[i] == freq2[j] && m1[j] && m2[i]){
-                    freq2[j] = 0;
-                    freq1[i] = 0;
-                }
-            }
-        }
-
-        for(int i=0;i<26;i++){
-            if(freq1[i] > 0 || freq2[i] > 0){
+            // If one has the character and the other doesn't, return false
+            if((freq1[i] > 0 && freq2[i] == 0) || (freq1[i] == 0 && freq2[i] > 0)){
                 return false;
             }
         }
 
-        return true;
+        sort(freq1.begin(),freq1.end());
+        sort(freq2.begin(),freq2.end());
+
+        return freq1 == freq2;
     }
 };
